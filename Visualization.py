dfc22a9d16667e2784e3563ef2335209b03ea493
vivekuk <60242797+vivekuk@users.noreply.github.com>
2023-12-06 23:51:51 -0500

    Delete Visualization.py

--- Visualization.py
+++
@@ -1,6 +0,0 @@
-# Visualization example: Scatter plot
-plt.scatter(df['Speciality'], df['kmeans_Cluster'])
-plt.title('Scatter Plot of Speciality vs kmeans_Cluster')
-plt.xlabel('Speciality')
-plt.ylabel('kmeans_Cluster')
-plt.show()
